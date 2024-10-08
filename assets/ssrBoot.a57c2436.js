b2e6ca688b456845d46249f75ecd6d08cd1651b6
margo-lee <margo.lee@hyweb.com.tw>
2024-10-08 11:02:31 +0800

    deploy

---
+++ assets/ssrBoot.a57c2436.js
@@ -0,0 +1,1 @@
+import{s as e,j as s,e as t,aq as n}from"./index.9036433b.js";function i(){const o=e(!1);return s(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:t(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
