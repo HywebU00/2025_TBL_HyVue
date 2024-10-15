12ccbb8b110043eacd76d04ffd512a331d98e2dd
margo-lee <margo.lee@hyweb.com.tw>
2024-10-15 15:08:38 +0800

    deploy

--- assets/EasyCard.c871f562.js
+++
@@ -1,1 +0,0 @@
-const r=""+new URL("idcard.9605cb29.svg",import.meta.url).href,a=""+new URL("EasyCard.6573ab9d.svg",import.meta.url).href;export{r as _,a};
--- assets/scopeId.55674790.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index.ed8fc8c6.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
