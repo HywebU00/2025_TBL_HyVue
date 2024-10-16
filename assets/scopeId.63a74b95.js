1596c3142f4e5b019e592bdd5fa80004a0b7de48
margo-lee <margo.lee@hyweb.com.tw>
2024-10-16 11:24:14 +0800

    deploy

---
+++ assets/scopeId.63a74b95.js
@@ -0,0 +1,1 @@
+import{h as o}from"./index.06904b0d.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ assets/ssrBoot.69373d5c.js
@@ -0,0 +1,1 @@
+import{s as e,j as s,e as t,ar as n}from"./index.06904b0d.js";function i(){const o=e(!1);return s(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:t(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
